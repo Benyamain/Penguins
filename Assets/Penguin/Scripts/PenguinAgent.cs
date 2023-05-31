e6151f7d41df7c75e319d025ce79d5519ea837f2
Benyamain <yacoobbenyamain@gmail.com>
2023-05-31 02:48:31 -0400

    Implementing the agent script

--- Assets/Penguin/Scripts/PenguinAgent.cs
+++ Assets/Penguin/Scripts/PenguinAgent.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
-public class PenguinAgent : MonoBehaviour
+public class PenguinAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // How fast the agent moves forward
+    public float moveSpeed = 5f;
+    // How fast the agent turns
+    public float turnSpeed = 180f;
+    // Heart prefab that appears when the baby is fed
+    public GameObject heartPrefab;
+    // Regurgitated fish prefab that appears when the baby is fed
+    public GameObject regurgitatedFishPrefab;
+    
+    private PenguinArea penguinArea;
+    new private Rigidbody rigidbody;
+    private GameObject baby;
+    // Depicts if the penguin has a full stomach
+    private bool isFull;
+
+    // When the agent wakes up (enables), not when it resets
+    public override void Initialize() {
+        base.Initialize();
+        penguinArea = GetComponentInParent<PenguinArea>();
+        baby = penguinArea.penguinBaby;
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // When the agent receives and responds to commands. These commands can originate from a neural network or a human player
+    // actionBuffers is a struct that contains an array of numerical values that correspond to actions the agent should take
+    // Discrete indicates that each numerical value corresponds to a singular action
+    // DiscreteActions[0]: 0 or 1 ; 0 to remain in place and 1 to move forward at full speed
+    // DiscreteActions[1]: 0, 1, 2 ; 0 to not turn, 1 to turn in the negative direction, and 2 to turn in the positive direction
+    // However, neural network has no concept of what these actions do. All it does know that some actions tend to result in more reward points
+    // Applies movement, rotation, and adds a small negative reward. The reward encourages the agent to complete its task ASAP
+    // Application example: -(steps / 5000). Longer it takes the agent, bigger the (-) value
+
+    // Performs actions based on vector of numbers
+    public override void OnActionReceived(ActionBuffers actionBuffers) {
+        // Con
     }
 }
--- Assets/Penguin/Scripts/PenguinArea.cs
+++ Assets/Penguin/Scripts/PenguinArea.cs
@@ -17,7 +17,8 @@ public class PenguinArea : Area
     private List<GameObject> fishList;
 
     // Reset the area which includes the fish and penguin placement
-    public void ResetArea() {
+    public void ResetArea()
+    {
         // Make sure no fish are in the area before spawning new fish
         RemoveAllFish();
         PlacePenguin();
@@ -26,13 +27,15 @@ public class PenguinArea : Area
     }
 
     // Remove a specific fish from the area when it is eaten
-    public void RemoveSpecificFish(GameObject fishObject) {
+    public void RemoveSpecificFish(GameObject fishObject)
+    {
         fishList.Remove(fishObject);
         Destroy(fishObject);
     }
 
     // The number of fish remaining
-    public int FishRemaining {
+    public int FishRemaining
+    {
         get { return fishList.Count; }
     }
 
@@ -40,16 +43,19 @@ public class PenguinArea : Area
     // Radius is distances from the center
     // Angle represents the angles of the wedge
     // Purpose: Use special radius and angle limits to pick a random position within wedges around the central point in the area
-    public static Vector3 ChooseRandomPosition(Vector3 center, float minAngle, float maxAngle, float minRadius, float maxRadius) {
+    public static Vector3 ChooseRandomPosition(Vector3 center, float minAngle, float maxAngle, float minRadius, float maxRadius)
+    {
         float radius = minRadius;
         float angle = minAngle;
 
-        if (maxRadius > minRadius) {
+        if (maxRadius > minRadius)
+        {
             // Pick a random radius
             radius = UnityEngine.Random.Range(minRadius, maxRadius);
         }
 
-        if (maxAngle > minAngle) {
+        if (maxAngle > minAngle)
+        {
             // Pick a random angle
             angle = UnityEngine.Random.Range(minAngle, maxAngle);
         }
@@ -58,9 +64,12 @@ public class PenguinArea : Area
         return center + Quaternion.Euler(0f, angle, 0f) * Vector3.forward * radius;
     }
 
-    public void RemoveAllFish() {
-        for (int i = 0; i < fishList.Count; i++) {
-            if (fishList[i] != null) {
+    public void RemoveAllFish()
+    {
+        for (int i = 0; i < fishList.Count; i++)
+        {
+            if (fishList[i] != null)
+            {
                 Destroy(fishList[i]);
             }
         }
@@ -69,8 +78,10 @@ public class PenguinArea : Area
     }
 
     // Place a penguin in the area
-    public void PlacePenguin() {
+    public void PlacePenguin()
+    {
         Rigidbody rigidbody = penguinAgent.GetComponent<Rigidbody>();
+        // Prevents the penguin from falling through the ground since unexpected occurrences can come about when training for long periods of time
         rigidbody.velocity = Vector3.zero;
         rigidbody.angularVelocity = Vector3.zero;
         penguinAgent.transform.position = ChooseRandomPosition(transform.position, 0f, 360f, 0f, 9f) + Vector3.up * .5f;
@@ -78,11 +89,47 @@ public class PenguinArea : Area
     }
 
     // Place the baby in the area
-    public void PlaceBaby() {
-        Rigidbody rigidbody =  penguinBaby.GetComponent<Rigidbody>();
+    public void PlaceBaby()
+    {
+        Rigidbody rigidbody = penguinBaby.GetComponent<Rigidbody>();
+        // Prevents the baby penguin from falling through the ground since unexpected occurrences can come about when training for long periods of time
         rigidbody.velocity = Vector3.zero;
         rigidbody.angularVelocity = Vector3.zero;
         penguinBaby.transform.position = ChooseRandomPosition(transform.position, -45f, 45f, 4f, 9f) + Vector3.up * .5f;
         penguinBaby.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+    }
+
+    public void SpawnFish(int count, float fishSpeed)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            // Spawn and place the fish
+            GameObject fishObject = Instantiate<GameObject>(fishPrefab.gameObject);
+            fishObject.transform.position = ChooseRandomPosition(transform.position, 100f, 260f, 2f, 13f) + Vector3.up * .5f;
+            fishObject.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+
+            // Set the fish's parent to this area's transform
+            fishObject.transform.SetParent(transform);
+
+            // Keep track of the fish
+            fishList.Add(fishObject);
+
+            // Set the fish speed
+            fishObject.GetComponent<Fish>().fishSpeed = fishSpeed;
+        }
+
+    }
+
+    // When game starts
+    private void Start()
+    {
+        ResetArea();
+    }
+
+    // Called every frame
+    private void Update()
+    {
+        // Update the reward text to see how well the penguins are performing
+        cumulativeRewardText.text = penguinAgent.GetCumulativeReward().ToString("0.00");
     }
 }
